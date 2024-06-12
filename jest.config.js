7dc644582330c3a6ad85bbfc912b86f3d1a4a473
Tobias Maciel <tobiasmaciel03@gmail.com>
2024-06-12 12:37:06 -0300

    fix

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['lcov', 'text', 'text-summary'],
+    collectCoverageFrom: ['src/FuncSum.js']
   };
   
